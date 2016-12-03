d9459ddaf9a6b5a81671d799c2eaaf50858d3aac
Brian Ault <baultik@gmail.com>
2016-12-02 19:05:05 -0800

    randomize array

--- practice20/answer.js
+++ practice20/answer.js
@@ -1,1 +1,21 @@
-answer.js
+function randomize(inputArray) {
+    var outputArray = [];
+    var size = inputArray.length;
+
+    while (outputArray.length < size) {
+        var placed = false;
+
+        while(!placed) {
+            var randIndex = Math.floor(Math.random() * size);
+            var item = inputArray[randIndex];
+
+            if (outputArray.indexOf(item) == -1) {
+                outputArray.push(item);
+                placed = true;
+            }
+        }
+
+    }
+
+    return outputArray;
+}
