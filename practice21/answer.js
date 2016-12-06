570806df75760cb51e5ea15c1fffa8d2f37ad3ef
Brian Ault <baultik@gmail.com>
2016-12-06 14:11:34 -0800

    practice 21

--- practice21/answer.js
+++ practice21/answer.js
@@ -1,1 +1,6 @@
-answer.js
+function bipolar(count) {
+    if (isNaN(count)) count = 10;
+    for (var i = 0, j = count; i < count; i++,j--) {
+        console.log("i = " + i + " j = " + j);
+    }
+}
