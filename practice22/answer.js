d7cda18df5fb11fab140d0faaf8b5187c0a85c9b
Brian Ault <baultik@gmail.com>
2016-12-15 17:20:02 -0800

    practice22

--- practice22/answer.js
+++ practice22/answer.js
@@ -1,1 +1,18 @@
-answer.js
+function sortObjectListByField(inputArray, sortField) {
+    var unsorted = true;
+    while(unsorted) {
+        var count = 0;
+        for (var i = 0; i < inputArray.length - 1; i++) {
+            var temp1 = inputArray[i];
+            var temp2 = inputArray[i+1];
+
+            if (temp1[sortField] > temp2[sortField]) {
+                inputArray[i] = temp2;
+                inputArray[i+1] = temp1;
+                count++;
+            }
+        }
+        if (!count) unsorted = false;
+    }
+    return inputArray;
+}
