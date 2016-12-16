398b80a3a6e5605522b3e3c541f9be8cbbedcff7
Brian Ault <baultik@gmail.com>
2016-12-15 17:41:42 -0800

    practice26

--- practice26/answer.js
+++ practice26/answer.js
@@ -1,1 +1,16 @@
-answer.js
+function searchArray(haystackArray, needleArray) {
+    var output = [];
+
+    for (var i in haystackArray) {
+        var hay = haystackArray[i];
+
+        for (var j in needleArray) {
+            var needle = needleArray[j];
+            if (hay === needle) {
+                output.push(needle);
+            }
+        }
+    }
+
+    return output;
+}
